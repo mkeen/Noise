862c10c9b2870ef36a2205c8258bfe8cdc3f32c6
Mike Keen <mkeen.atl@gmail.com>
2012-04-08 17:36:25 -0400

    made example better

--- examples/basic_client.rb
+++ examples/basic_client.rb
@@ -1,15 +1,22 @@
-require "rubygems"
-require "noise"
+require "../lib/noise.rb"
 
 Noise::Connection.new :host => "localhost", :port => 61613, :user => "guest", :pass => "guest" do
-	subscribe "/topic/mike"
+  # At this point, you are connected to the server and the login you specified has been verified.
+  subscribe "/topic/noise"
 
-	on_message do |frame|
-		puts frame.to_hash[:body]
-		send rand(10000).to_s, "/topic/mike"
-	end
+  on_message do |frame|
+    puts frame.to_hash[:body]
+    send rand(10000).to_s, "/topic/noise"
+  end
 
-	send "this is a test of the emergency broadcasting system", "/topic/mike"
+  on "message", false do |frame|
+    puts "received a message. this event callback will only fire once"
+  end
+
+  send "this is a test of the emergency broadcasting system", "/topic/noise"
 end
 
-sleep 5000
+# You will need to keep the process alive somehow in order to listen for incoming messages.
+# You can do better than this:
+
+sleep 500
